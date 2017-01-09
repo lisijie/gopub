c21c93b0adcf3877cde3363ecf29a4ce8b0b227a
jesse.li <lsj86@qq.com>
2017-01-09 17:45:33 +0800

    更新

--- app/entity/role.go
+++ app/entity/role.go
@@ -7,13 +7,13 @@ import (
 // 角色
 type Role struct {
     Id          int
-    RoleName    string    // 角色名称
-    ProjectIds  string    // 项目权限
-    Description string    // 说明
-    CreateTime  time.Time // 创建时间
-    UpdateTime  time.Time // 更新时间
-    PermList    []Perm    // 权限列表
-    UserList    []User    // 用户列表
+    RoleName    string           // 角色名称
+    ProjectIds  string           // 项目权限
+    Description string           // 说明
+    CreateTime  time.Time        // 创建时间
+    UpdateTime  time.Time        // 更新时间
+    PermList    []Perm `orm:"-"` // 权限列表
+    UserList    []User `orm:"-"` // 用户列表
 }
 
 // 角色权限
@@ -21,3 +21,4 @@ type RolePerm struct {
     RoleId int    // 角色id
     Perm   string // 权限
 }
+
